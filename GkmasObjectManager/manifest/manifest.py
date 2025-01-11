8e41bd5fd52f02b6c674fbdac9da8912df7ec149
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-01-10 22:26:51 -0500

    [Style] Merge GkmasManifest class splits
    ... after the majority of manifest/_initdb was moved to __init__

--- GkmasObjectManager/manifest/manifest.py
+++ GkmasObjectManager/manifest/manifest.py
@@ -3,27 +3,26 @@ manifest.py
 Manifest decryption, exporting, and object downloading.
 """
 
+from ..object import GkmasAssetBundle, GkmasResource
 from ..log import Logger
-from ..const import PATH_ARGTYPE, DICLIST_IGNORED_FIELDS
-
-
-from .utils import Diclist
 from ..const import (
     PATH_ARGTYPE,
-    GKMAS_API_URL,
-    GKMAS_API_HEADER,
-    GKMAS_ONLINEPDB_KEY,
-    GKMAS_OCTOCACHE_KEY,
-    GKMAS_OCTOCACHE_IV,
+    ALL_ASSETBUNDLES,
+    ALL_RESOURCES,
+    IMG_RESIZE_ARGTYPE,
+    DEFAULT_DOWNLOAD_PATH,
+    DEFAULT_DOWNLOAD_NWORKER,
+    DICLIST_IGNORED_FIELDS,
+    CSV_COLUMNS,
 )
 
-from .decrypt import AESCBCDecryptor
-from ..object import GkmasAssetBundle, GkmasResource
+from .octodb_pb2 import dict2pdbytes
+from .utils import Diclist, ConcurrentDownloader
 
-import requests
+import re
+import json
+import pandas as pd
 from pathlib import Path
-from urllib.parse import urljoin
-from google.protobuf.json_format import MessageToDict
 
 
 # The logger would better be a global variable in the
@@ -55,12 +54,6 @@ class GkmasManifest:
         export(path: Union[str, Path]) -> None:
             Exports the manifest as ProtoDB, JSON, and/or CSV to the specified path.
     """
-
-    # It's necessary to import all methods instead of merely interface/dispatcher functions;
-    # otherwise, self._helper_method() in these interface functions would encounter name
-    # resolution errors. Also, import * is prohibited unless importing from a module.
-    from ._download import download
-    from ._export import export, _export_pdb, _export_json, _export_csv
 
     def __init__(self, jdict: dict):
         """
@@ -124,3 +117,135 @@ class GkmasManifest:
         )
         logger.info("Manifest created from differentiation")
         return manifest
+
+    # ------------ EXPORT ------------ #
+
+    def export(self, path: PATH_ARGTYPE):
+        """
+        Exports the manifest as ProtoDB, JSON, and/or CSV to the specified path.
+        This is a dispatcher method.
+
+        Args:
+            path (Union[str, Path]): A directory or a file path.
+                If a directory, all three formats are exported.
+                If a file path, the format is determined by the extension
+                (all extensions other than .json and .csv are treated as raw binary
+                and therefore exported as ProtoDB).
+        """
+
+        path = Path(path)
+
+        if path.suffix == "":
+            # used to be path.is_dir(), but it also returns False for non-existent dirs
+            path.mkdir(parents=True, exist_ok=True)
+            self._export_pdb(path / f"manifest_v{self.revision}.pdb")
+            self._export_json(path / f"manifest_v{self.revision}.json")
+            self._export_csv(path / f"manifest_v{self.revision}.csv")
+
+        else:
+            path.parent.mkdir(parents=True, exist_ok=True)
+            if path.suffix == ".pdb":
+                self._export_pdb(path)
+            elif path.suffix == ".json":
+                self._export_json(path)
+            elif path.suffix == ".csv":
+                self._export_csv(path)
+            else:
+                logger.warning("Unrecognized file extension, abort")
+
+    def _export_pdb(self, path: Path):
+        """
+        [INTERNAL] Writes raw protobuf bytes into the specified path.
+        """
+        try:
+            path.write_bytes(dict2pdbytes(self.jdict))
+            logger.success(f"ProtoDB has been written into {path}")
+        except:
+            logger.warning(f"Failed to write ProtoDB into {path}")
+
+    def _export_json(self, path: Path):
+        """
+        [INTERNAL] Writes JSON-serialized dictionary into the specified path.
+        """
+        try:
+            path.write_text(json.dumps(self.jdict, indent=4))
+            logger.success(f"JSON has been written into {path}")
+        except:
+            logger.warning(f"Failed to write JSON into {path}")
+
+    def _export_csv(self, path: Path):
+        """
+        [INTERNAL] Writes CSV-serialized data into the specified path.
+        Assetbundles and resources are concatenated into a single table and sorted by name.
+        Assetbundles can be distinguished by their '.unity3d' suffix.
+        """
+        dfa = pd.DataFrame(self._abl, columns=CSV_COLUMNS)
+        dfa["name"] = dfa["name"].apply(lambda x: x + ".unity3d")
+        dfr = pd.DataFrame(self._resl, columns=CSV_COLUMNS)
+        df = pd.concat([dfa, dfr], ignore_index=True)
+        df.sort_values("name", inplace=True)
+        try:
+            df.to_csv(path, index=False)
+            logger.success(f"CSV has been written into {path}")
+        except:
+            logger.warning(f"Failed to write CSV into {path}")
+
+    # ----------- DOWNLOAD ----------- #
+
+    def download(
+        self,
+        *criteria: str,
+        nworker: int = DEFAULT_DOWNLOAD_NWORKER,
+        path: PATH_ARGTYPE = DEFAULT_DOWNLOAD_PATH,
+        categorize: bool = True,
+        extract_img: bool = True,
+        img_format: str = "png",
+        img_resize: IMG_RESIZE_ARGTYPE = None,
+    ):
+        """
+        Downloads the regex-specified assetbundles/resources to the specified path.
+
+        Args:
+            *criteria (str): Regex patterns of assetbundle/resource names.
+                Allowed special tokens are const.ALL_ASSETBUNDLES and const.ALL_RESOURCES.
+            nworker (int) = DEFAULT_DOWNLOAD_NWORKER: Number of concurrent download workers.
+                Defaults to multiprocessing.cpu_count().
+            path (Union[str, Path]) = DEFAULT_DOWNLOAD_PATH: A directory to which the objects are downloaded.
+                *WARNING: Behavior is undefined if the path points to an definite file (with extension).*
+            categorize (bool) = True: Whether to categorize the downloaded objects into subdirectories.
+                If False, all objects are downloaded to the specified 'path' in a flat structure.
+            extract_img (bool) = True: Whether to extract images from assetbundles of type 'img'.
+                If False, 'img_.*\\.unity3d' are downloaded as is.
+            img_format (str) = 'png': Image format for extraction. Case-insensitive.
+                Effective only when 'extract_img' is True. Format must support RGBA mode.
+                Valid options are checked by PIL.Image.save() and are not enumerated.
+            img_resize (Union[None, str, Tuple[int, int]]) = None: Image resizing argument.
+                If None, images are downloaded as is.
+                If str, string must contain exactly one ':' and images are resized to the specified ratio.
+                If Tuple[int, int], images are resized to the specified exact dimensions.
+        """
+
+        objects = []
+
+        for criterion in criteria:
+            if criterion == ALL_ASSETBUNDLES:  # special tokens, enclosed in <>
+                objects.extend(self.abs)
+            elif criterion == ALL_RESOURCES:
+                objects.extend(self.reses)
+            else:
+                objects.extend(
+                    [
+                        self._name2object[file]
+                        for file in self._name2object
+                        if re.match(criterion, file)
+                    ]
+                )
+
+        ConcurrentDownloader(nworker).dispatch(
+            objects,
+            path=path,
+            categorize=categorize,
+            extract_img=extract_img,
+            img_format=img_format,
+            img_resize=img_resize,
+        )
