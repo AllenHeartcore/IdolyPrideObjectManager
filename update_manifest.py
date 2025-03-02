9b9ab0451eb335a214c2b1e4ea9d8173d1d49aa6
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-01 19:18:16 -0500

    Draft update_manifest.py for workflow

---
+++ update_manifest.py
@@ -0,0 +1,28 @@
+"""
+update_manifest.py
+Script to fetch latest manifest and diff from server,
+compatible with 'Update Manifest' workflow.
+"""
+
+import GkmasObjectManager as gom
+
+
+def main():
+
+    m_remote = gom.fetch()
+    m_local = gom.load("manifests/v0000.json")
+    if m_remote.revision == m_local.revision:
+        print("No update available.")
+        return
+
+    for i in range(m_remote.revision._get_canon_repr()):
+        try:
+            gom.fetch(i).export(f"manifests/v{i:04}.json")
+        except KeyError:
+            # would probably fail at (revision - 1),
+            # signaled by a "KeyError: 'resourceList'"
+            break
+
+
+if __name__ == "__main__":
+    main()
