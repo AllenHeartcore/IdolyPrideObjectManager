669eeab52f26935389340aca453bcdade40821b1
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-01 20:28:33 -0500

    [Fix] Read local version from file; complete workflow (conditional commit)

--- update_manifest.py
+++ update_manifest.py
@@ -7,26 +7,25 @@ compatible with 'Update Manifest' workflow.
 import GkmasObjectManager as gom
 
 import sys
+from pathlib import Path
 
 
 def main():
 
     m_remote = gom.fetch()
-    latest_revision = m_remote.revision._get_canon_repr()
+    rev_remote = m_remote.revision._get_canon_repr()
+    rev_local = int(Path("manifests/LATEST_REVISION").read_text())
 
-    m_local = gom.load("manifests/v0000.json")
-    if m_remote.revision == m_local.revision:
+    if rev_remote == rev_local:
         print("No update available.")
         sys.exit(-1)
 
     # Only write to file after sanity check;
-    # this number is only used to construct commit message in workflow
-    # but not used to determine local manifest revision.
-    with open("manifests/LATEST_REVISION", "w") as f:
-        f.write(str(latest_revision))
+    # this number is used to construct commit message in workflow.
+    Path("manifests/LATEST_REVISION").write_text(str(rev_remote))
 
     m_remote.export(f"manifests/v0000.json")
-    for i in range(1, latest_revision):
+    for i in range(1, rev_remote):
         gom.fetch(i).export(f"manifests/v{i:04}.json")
 
     sys.exit(0)
