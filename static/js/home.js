ea64f51f86232fc8ee0b731c5f81f15d72005501
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-04 02:36:53 -0500

    (GUI) Draft regex check to display sample 'landscape wallpaper' images in homepage
    Byproducts: revised namecard kit and "revived" wallpaper kit schemes.

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,29 @@
+const WALLPAPER_REGEX_PATTERN = /img_general_(cidol|csprt|meishi_base).*full.*/;
+
+function populateHomepageContainers(data) {
+    $("#loadingSpinner").hide();
+
+    $("#homeMetadata").show();
+    $("#homeMetadataRevision").text(data.revision);
+    $("#homeMetadataAbCount").text(data.assetBundleList.length);
+    $("#homeMetadataResCount").text(data.resourceList.length);
+
+    // for each asset bundle, check if matches the wallpaper regex
+    // if it does, add it to the carousel
+
+    // matches is empty list
+    // let matches = [];
+    // data.assetBundleList.forEach((ab) => {
+    //     if (WALLPAPER_REGEX_PATTERN.test(ab.name)) {
+    //         matches.push(ab.name);
+    //     }
+    // });
+    let matches = data.assetBundleList
+        .filter((ab) => WALLPAPER_REGEX_PATTERN.test(ab.name))
+        .map((ab) => ab.name);
+    console.log(matches);
+}
+
 $(document).ready(function () {
     $.ajax({
         type: "GET",
@@ -5,11 +31,7 @@ $(document).ready(function () {
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function (result) {
-            $("#loadingSpinner").hide();
-            $("#homeMetadata").show();
-            $("#homeMetadataRevision").text(result.revision);
-            $("#homeMetadataAbCount").text(result.assetBundleList.length);
-            $("#homeMetadataResCount").text(result.resourceList.length);
+            populateHomepageContainers(result);
         },
         error: function (request, status, error) {
             console.log("Error");
