f002e3b8f8787471a4c959fed0c5a5afd5b942d4
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-05 01:15:37 -0500

    (DRAFT) Debug vgmstream call in GkmasAWBAudio

--- GkmasObjectManager/media/audio.py
+++ GkmasObjectManager/media/audio.py
@@ -39,20 +39,21 @@ class GkmasAudio(GkmasDummyMedia):
 
 class GkmasAWBAudio(GkmasAudio):
 
-    def __init__(
-        self,
-        name: str,
-        data: bytes,
-    ):
+    def __init__(self, name: str, data: bytes):
         """
         Initializes **one** AWB audio from raw resource bytes.
         Raises a warning and falls back to raw dump if the archive contains multiple tracks.
         """
 
-        self.valid = True
-        self.name = name
-        self.io = BytesIO(data)
-        self.io.seek(0)
+        super().__init__(name, data)
+
+        self.obj = BytesIO(data)
+        self.obj.seek(0)
+        # It would be more reasonable to let self.obj be an AudioSegment,
+        # but the AWB format is not directly supported by pydub.
+
+        # Overwriting self.obj at conversion-enabled export is also bad for consistency, ...
+        self.wav = None  # ... but having a separate attribute is a valid workaround.
 
     def _get_embed_url(self) -> str:
         return ""
@@ -60,15 +61,28 @@ class GkmasAWBAudio(GkmasAudio):
     def export(
         self,
         path: Path,
-        audio_format: str,
+        extract_audio: bool = True,
+        audio_format: str = "wav",
     ):
         """
         Attempts to extract a single audio track from the archive.
+
+        Args:
+            extract_audio (bool) = True: Whether to extract a single audio track from the archive.
+                If False, 'sud_.*\\.awb/acb' is downloaded as is.
+                There are also cases where the audio is readily encoded in MP3.
+            audio_format (str) = 'wav': Audio format for extraction. Case-insensitive.
+                Effective only when 'extract_audio' is True.
+                Valid options are checked by pydub.AudioSegment.export() and are not enumerated.
         """
 
+        if not (self.valid and extract_audio):
+            super().export(path)
+
         subprocess.run(
-            "vgmstream",
-            input=self.io,
-            stdout=path.open("wb"),
+            "media/vgmstream/vgmstream",
+            input=self.obj.read(),
+            stdout=Path(path).with_suffix(".wav").open("wb"),
+            stderr=subprocess.PIPE,
             check=True,
         )
