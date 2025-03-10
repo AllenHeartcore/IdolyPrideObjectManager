43834a1dfe1a571f877da6b53bcbd9edc3e96fa0
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-09 20:15:23 -0400

    (GUI) [Refactor] Return a promise in getMediaBlobURL() to ensure synchronous activity
    Also fixes the behavior of displayCaption() with a similar style

--- static/js/utils.js
+++ static/js/utils.js
@@ -30,20 +30,30 @@ function enterKeyOverriderFactory(form_id) {
     };
 }
 
-function getMediaBlob(type, id) {
-    $.ajax({
-        type: "GET",
-        url: `/api/${type.toLowerCase()}/${id}/bytestream`,
-        xhrFields: { responseType: "arraybuffer" },
-    })
-        .then(function (data, status, request) {
-            const mimetype = request.getResponseHeader("Content-Type");
-            return new Blob([data], { type: mimetype });
-        })
-        .catch(function (...args) {
-            dumpErrorToConsole(...args);
-            return new Blob(["An error occurred while fetching media."], {
-                type: "text/plain",
-            });
+function getMediaBlobURL(type, id) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: `/api/${type.toLowerCase()}/${id}/bytestream`,
+            xhrFields: { responseType: "arraybuffer" },
+            success: function (data, status, request) {
+                const mimetype = request.getResponseHeader("Content-Type");
+                blob = new Blob([data], { type: mimetype });
+                resolve({
+                    url: URL.createObjectURL(blob),
+                    mimetype: mimetype,
+                });
+            },
+            error: function (...args) {
+                dumpErrorToConsole(...args);
+                blob = new Blob(["An error occurred while fetching media."], {
+                    type: "text/plain",
+                });
+                reject({
+                    url: URL.createObjectURL(blob),
+                    mimetype: "text/plain",
+                });
+            },
         });
+    });
 }
--- static/js/view.js
+++ static/js/view.js
@@ -1,62 +1,63 @@
 function displayMedia() {
-    $("#loadingSpinnerMedia").hide();
-    $("#viewMediaContent").show();
-    let container = $("#viewMediaContent");
+    getMediaBlobURL(type, info.id).then(({ url, mimetype }) => {
+        $("#loadingSpinnerMedia").hide();
+        $("#viewMediaContent").show();
+        let container = $("#viewMediaContent");
 
-    const blob = getMediaBlob(type, info.id);
-    const url = URL.createObjectURL(blob);
-    const mimetype = blob.type;
+        if (mimetype.startsWith("image/")) {
+            container.append($("<img>").attr("src", url));
+        } else if (mimetype.startsWith("audio/")) {
+            container.append($("<audio>").attr({ src: url, controls: true }));
+        } else if (mimetype.startsWith("video/")) {
+            container.append($("<video>").attr({ src: url, controls: true }));
+        } else {
+            container.append(
+                $("<a>")
+                    .attr("href", url)
+                    .text("Download Media")
+                    .attr("download", "")
+            );
+        }
 
-    if (mimetype.startsWith("image/")) {
-        container.append($("<img>").attr("src", url));
-    } else if (mimetype.startsWith("audio/")) {
-        container.append($("<audio>").attr({ src: url, controls: true }));
-    } else if (mimetype.startsWith("video/")) {
-        container.append($("<video>").attr({ src: url, controls: true }));
-    } else {
-        container.append(
+        $("#downloadLinks").append(
+            "&emsp; | &emsp;",
             $("<a>")
                 .attr("href", url)
-                .text("Download Media")
-                .attr("download", "")
+                .attr("download", info.name.replace(/\.[^/.]+$/, ""))
+                .text("Converted media")
         );
-    }
-
-    $("#downloadLinks").append(
-        "&emsp; | &emsp;",
-        $("<a>")
-            .attr("href", url)
-            .attr("download", info.name.replace(/\.[^/.]+$/, ""))
-            .text("Converted media")
-    );
+    });
 }
 
 function getCaption() {
-    $.ajax({
-        type: "GET",
-        url: `/api/${type.toLowerCase()}/${info.id}/caption`,
-        dataType: "text",
-        contentType: "charset=utf-8",
-        success: function (caption) {
-            return caption;
-        },
-        error: function (...args) {
-            dumpErrorToConsole(...args);
-            return "[An error occurred while generating caption.]";
-        },
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: `/api/${type.toLowerCase()}/${info.id}/caption`,
+            dataType: "text",
+            contentType: "charset=utf-8",
+            success: function (caption) {
+                resolve(caption);
+            },
+            error: function (...args) {
+                dumpErrorToConsole(...args);
+                reject("[An error occurred while generating caption.]");
+            },
+        });
     });
 }
 
 function displayCaption() {
-    let caption = getCaption();
-    $("#loadingSpinnerCaption").hide();
-    $("#viewCaptionText").show();
-    $("#viewCaptionText").text(caption);
+    getCaption().then((caption) => {
+        $("#loadingSpinnerCaption").hide();
+        $("#viewCaptionText").show();
+        $("#viewCaptionText").text(caption);
+    });
 }
 
 $(document).ready(function () {
     setAccentColorByString(info.name);
 
     displayMedia();
-    getCaption();
+    displayCaption();
 });
