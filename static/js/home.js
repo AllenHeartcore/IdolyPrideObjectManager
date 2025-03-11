5037356dd70d02d4bb43a4137b6da03b4db297ed
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-11 18:18:28 -0400

    (GUI) [Style/Minor] Display images with rounded corners in homepage
    Again, this is in fact responsive...

--- static/js/home.js
+++ static/js/home.js
@@ -18,7 +18,9 @@ function populateHomepageContainers(data) {
     // Pre-populate with empty divs for indexing
     let container = $("#homeFeaturedContainer");
     for (let i = 0; i < NUM_FEATURED_SAMPLES; i++) {
-        container.append($("<div>").addClass("col-md-2 mt-3 image-landscape"));
+        container.append(
+            $("<div>").addClass("col-md-2 mt-3 image-landscape image-roundedge")
+        );
     }
 
     // Place images in the correct order since Promise's are async
