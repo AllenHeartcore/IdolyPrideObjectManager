626e1b07488a01d0f0b700a2a8a66f90b233f924
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-12 01:09:22 -0400

    Switch to serial ID in sample manifest for CRUD compatibility

--- _make_manifest_sample.py
+++ _make_manifest_sample.py
@@ -2,18 +2,17 @@ import GkmasObjectManager as gom
 from GkmasObjectManager.const import GKMAS_OBJECT_SERVER
 
 import json
-from tqdm import tqdm
 from urllib.parse import urljoin
 
 
-# m = gom.fetch()
-m = gom.load("v199.json")
+m = gom.fetch()
+# m = gom.load("v199.json")
 resourceList = []
 
 # sud_music_general_all-001-amao_game.mp3 -> img_general_music_jacket_all-amao-001.png
 # sud_music_general_ttmr-003-ttmr_game.mp3 -> img_general_music_jacket_char-ttmr-003.png
 
-for obj in tqdm(m.search(".*.mp3$")):
+for obj in m.search(".*.mp3$"):
 
     segs = obj.name.split("-")
     songid_head = segs[0].split("_")[-1]
@@ -40,6 +39,8 @@ for obj in tqdm(m.search(".*.mp3$")):
 
 revision = m.revision._get_canon_repr()
 resourceList = sorted(resourceList, key=lambda x: x["id"])
+for i in range(len(resourceList)):
+    resourceList[i]["id"] = i + 1  # switch to serial ID for compatibility with CRUD app
 
 with open(f"manifest_v{revision}.json", "w") as f:
     json.dump(
