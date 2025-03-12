b03426d8ec1747b250196f23c20739fbacd7994d
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-11 20:23:02 -0400

    (GUI) Implement pagination on search page
    Also write detailed comments about control flow, and rename populateCardContainer() to updateCardContainer() to reflect the dependency (only page init is called 'populate')
    Also populate title & search input with beautified query string (trimmed, duplicate spaces removed)

--- static/js/search.js
+++ static/js/search.js
@@ -5,9 +5,104 @@ let sortState = {
     ascending: true,
 }; // backend sorts by ascending name
 
-function populateCardContainer() {
+// pagination support
+const ENTRIES_PER_PAGE = 12;
+const PAGE_NAV_CONTEXT_SIZE = 1;
+var currentPage = 1;
+var totalPages = 0;
+
+/*  CONTROL FLOW:
+    $(document).ready -> populateSearchpageContainers -> updateCardContainer
+    updateSort -> sortSearchEntries, updateCardContainer
+    updateCardContainer -> updatePagination -> appendPaginationButton
+
+    [init] - PSC - UCC - UP - APB
+                 /
+              US
+                 \
+                   SSE
+*/
+
+function appendPaginationButton(text, isEnabled, pageUpdater) {
+    $("#paginationContainer").append(
+        $("<button>")
+            .addClass("btn btn-primary mx-1")
+            .text(text)
+            .prop("disabled", !isEnabled)
+            .click(() => {
+                currentPage = pageUpdater(currentPage);
+                updateCardContainer();
+            })
+    );
+}
+
+function updatePagination() {
+    totalPages = Math.ceil(searchEntries.length / ENTRIES_PER_PAGE);
+    $("#paginationContainer").empty();
+
+    // Prev  [1]    2                      ...    N   Next
+    // Prev   1    [2]    3                ...    N   Next
+    // Prev   1     2    [3]    4          ...    N   Next
+    // Prev   1    ...    3    [4]    5    ...    N   Next
+    // Prev   1    ...   I-1   [I]   I+1   ...    N   Next
+    // Prev   1    ...   N-4  [N-3]  N-2   ...    N   Next
+    // Prev   1    ...         N-3  [N-2]  N-1    N   Next
+    // Prev   1    ...               N-2  [N-1]   N   Next
+    // Prev   1    ...                     N-1   [N]  Next
+
+    // head
+    appendPaginationButton("Prev", currentPage > 1, (page) => page - 1);
+    appendPaginationButton("1", currentPage !== 1, () => 1);
+
+    if (totalPages <= PAGE_NAV_CONTEXT_SIZE * 2 + 1) {
+        // no need for ellipsis
+        for (let i = 2; i < totalPages; i++) {
+            appendPaginationButton(i.toString(), currentPage !== i, () => i);
+        }
+    } else {
+        // leading ellipsis
+        if (currentPage > PAGE_NAV_CONTEXT_SIZE + 2) {
+            $("#paginationContainer")
+                .append($("<span>").text("..."))
+                .addClass("mx-2");
+        }
+
+        // context
+        let start = Math.max(currentPage - PAGE_NAV_CONTEXT_SIZE, 2);
+        let end = Math.min(currentPage + PAGE_NAV_CONTEXT_SIZE, totalPages - 1);
+        for (let i = start; i <= end; i++) {
+            appendPaginationButton(i.toString(), currentPage !== i, () => i);
+        }
+
+        // trailing ellipsis
+        if (currentPage < totalPages - PAGE_NAV_CONTEXT_SIZE - 1) {
+            $("#paginationContainer")
+                .append($("<span>").text("..."))
+                .addClass("mx-2");
+        }
+    }
+
+    // tail
+    appendPaginationButton(
+        totalPages.toString(),
+        currentPage !== totalPages,
+        () => totalPages
+    );
+    appendPaginationButton(
+        "Next",
+        currentPage < totalPages && totalPages > 0,
+        (page) => page + 1
+    );
+}
+
+function updateCardContainer() {
     $("#searchEntryCardContainer").empty();
-    searchEntries.forEach((entry) => {
+
+    let start = (currentPage - 1) * ENTRIES_PER_PAGE;
+    let end = Math.min(currentPage * ENTRIES_PER_PAGE, searchEntries.length);
+    let pageEntries = searchEntries.slice(start, end);
+
+    pageEntries.forEach((entry) => {
         let card = $("<div>").addClass("card").attr("id", "searchEntryCard");
         if (entry.name.startsWith("img_")) {
             getMediaBlobURL(entry.type, entry.id).then(({ url, mimetype }) => {
@@ -36,6 +131,8 @@ function populateCardContainer() {
             $("<div>").addClass("col-md-3").append(card)
         );
     });
+
+    updatePagination();
 }
 
 function sortSearchEntries() {
@@ -59,17 +156,19 @@ function sortSearchEntries() {
 function updateSort() {
     let byID_new = $("#sortByID").is(":checked");
     let ascending_new = $("#sortAsc").is(":checked");
-    console.log(byID_new, ascending_new);
     if (byID_new === sortState.byID && ascending_new === sortState.ascending) {
         return;
     }
+    currentPage = 1;
     sortState.byID = byID_new;
     sortState.ascending = ascending_new;
     sortSearchEntries();
-    populateCardContainer();
+    updateCardContainer();
 }
 
-function populateSearchpageContainers() {
+function populateSearchpageContainers(queryDisplay) {
+    $("#searchResultTitle").text(`Search results for "${queryDisplay}"`);
+
     if (searchEntries.length === 0) {
         $("#searchResultDigest").text("No results found.");
     } else {
@@ -77,7 +176,7 @@ function populateSearchpageContainers() {
             `Found ${searchEntries.length}` +
                 (searchEntries.length === 1 ? " entry." : " entries.")
         );
-        populateCardContainer();
+        updateCardContainer();
     }
 
     $("#loadingSpinner").hide();
@@ -85,7 +184,9 @@ function populateSearchpageContainers() {
 }
 
 $(document).ready(function () {
-    $("#searchInput").val(query); // allows immediate edit/resubmission
+    let queryDisplay = query.trim().replace(/\s+/g, " "); // trimmed, duplicate spaces removed
+    $("#searchInput").val(queryDisplay + " "); // allows immediate edit/resubmission
+    // search input should be displayed alongside the spinner, before a successful AJAX response
 
     $.ajax({
         type: "GET",
@@ -95,7 +196,7 @@ $(document).ready(function () {
         contentType: "application/json; charset=utf-8",
         success: function (result) {
             searchEntries = result;
-            populateSearchpageContainers();
+            populateSearchpageContainers(queryDisplay); // an extra arg, fine
         },
         error: function (...args) {
             dumpErrorToConsole(...args);
