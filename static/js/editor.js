5543b97afbc9870ecef3ce4307665da7b53c4a36
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-14 12:15:18 -0400

    (DRAFT) Implement editorpage layout for URL preview & keywords list

--- static/js/editor.js
+++ static/js/editor.js
@@ -35,33 +35,44 @@ $(document).ready(function () {
     displayCaption();
 
     $("#editorFieldSongUrl").on("input", function () {
-        let objectName = $(this).val();
-        if (objectName === "") {
-            $("#editorMediaAudio").attr("src", info.url);
+        let url =
+            "https://object.asset.game-gakuen-idolmaster.jp/" + $(this).val();
+        $("#editorMediaAudio").attr("src", url);
+
+        if ($(this).val() !== info.url.slice(-6)) {
+            $("#editorTableSize").text("Changed");
+            $("#editorTableMD5").text("Changed");
         } else {
-            $("#editorMediaAudio").attr(
-                "src",
-                "https://object.asset.game-gakuen-idolmaster.jp/" + objectName
-            );
+            $("#editorTableSize").text(info.size);
+            $("#editorTableMD5").text(info.md5);
         }
     });
 
     $("#editorFieldCoverUrl").on("input", function () {
-        let objectName = $(this).val();
-        if (objectName === "") {
-            $("#editorMediaImage").attr("src", info.cover);
-        } else {
-            $("#editorMediaImage").attr(
-                "src",
-                "https://object.asset.game-gakuen-idolmaster.jp/" + objectName
-            );
-        }
+        let url =
+            "https://object.asset.game-gakuen-idolmaster.jp/" + $(this).val();
+        $("#editorMediaImage").attr("src", url);
     });
 
+    // gray rectangle filler for invalid url
     $("#editorMediaImage").on("error", function () {
         $(this).attr(
             "src",
-            "data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><rect width='100%' height='100%' fill='%23cccccc'/></svg>"
+            "data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><rect width='100%' height='100%' fill='%23dddddd'/></svg>"
         );
+    });
+
+    $("#editorKeywordsAddButton").on("click", function () {
+        $("#keywords-list-ul").append(
+            `<li class="mb-2 d-flex">
+                <input type="text" class="fs-4 form-control me-2" />
+                <button id="editorKeywordsDeleteButton" type="button" class="fs-4 btn btn-danger bi bi-trash"></button>
+            </li>`
+        );
+    });
+
+    // event delegation for dynamically added elements
+    $(document).on("click", "#editorKeywordsDeleteButton", function () {
+        $(this).parent().remove();
     });
 });
