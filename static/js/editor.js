95e1cb9984823a86c1d46081ec50150ba1eb90a4
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-03-14 03:40:24 -0400

    (DRAFT) Sketch responsive song/cover URL form

--- static/js/editor.js
+++ static/js/editor.js
@@ -29,9 +29,39 @@ $(document).ready(function () {
         let alias = CHARACTER_ALIAS[keyword];
         if (alias) {
             setAccentColorByString(alias);
-            // there's still a tiny delay
         }
     }
 
     displayCaption();
+
+    $("#editorFieldSongUrl").on("input", function () {
+        let objectName = $(this).val();
+        if (objectName === "") {
+            $("#editorMediaAudio").attr("src", info.url);
+        } else {
+            $("#editorMediaAudio").attr(
+                "src",
+                "https://object.asset.game-gakuen-idolmaster.jp/" + objectName
+            );
+        }
+    });
+
+    $("#editorFieldCoverUrl").on("input", function () {
+        let objectName = $(this).val();
+        if (objectName === "") {
+            $("#editorMediaImage").attr("src", info.cover);
+        } else {
+            $("#editorMediaImage").attr(
+                "src",
+                "https://object.asset.game-gakuen-idolmaster.jp/" + objectName
+            );
+        }
+    });
+
+    $("#editorMediaImage").on("error", function () {
+        $(this).attr(
+            "src",
+            "data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'><rect width='100%' height='100%' fill='%23cccccc'/></svg>"
+        );
+    });
 });
