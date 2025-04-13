ede0a908f213cead8927723b0068b4a17f478716
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-04-13 15:36:31 -0400

    [Refactor] Implement CacheHandler in sovits_dataset
    SudCacheHandler (a children) takes over cache init, diff, download, read, and purge.

--- sovits_dataset.py
+++ sovits_dataset.py
@@ -11,11 +11,79 @@ import GkmasObjectManager as gom
 from GkmasObjectManager.log import Logger
 
 
+logger = Logger()
+logger.info("Fetching manifest...")
+m = gom.fetch()
+
+
+class CacheHandler:
+
+    def __init__(self, cwd: Path, args=None):
+        self.cwd = cwd
+        self.args = args
+        self.active = False
+
+        if self.cwd.exists():
+            assert self.cwd.is_dir(), f"{self.cwd} is not a directory"
+            if list(self.cwd.iterdir()):
+                self.active = True
+        else:
+            self.cwd.mkdir(parents=True, exist_ok=True)
+
+    def _rectify_filename(self, p: Path) -> str:
+        return p.name
+
+    def cache(self, target: list[str]):
+        target = set(target)  # remove duplicates
+        if self.active:
+            target -= set(map(self._rectify_filename, self.cwd.iterdir()))
+        m.download(
+            *sorted(list(target)),  # sort for logging
+            path=self.cwd,
+            categorize=False,
+            convert_audio=True,
+            audio_format="wav",  # avoid double compression
+            unpack_subsongs=True,
+        )
+
+    def read(self, filename: str) -> bytes:
+        return (self.cwd / filename).read_bytes()
+
+    def purge(self):
+        if self.args.keep_cache or self.active:
+            return
+        for f in tqdm(self.cwd.iterdir()):
+            f.unlink()
+        shutil.rmtree(self.cwd)
+
+
+class SudCacheHandler(CacheHandler):
+
+    def __init__(self, cwd: Path, args=None):
+        super().__init__(cwd, args)
+
+    def _rectify_filename(self, p: Path) -> str:
+        f = p.name
+        if f.startswith("sud_vo_adv_"):
+            f = "_".join(f.split("_")[:-1])
+        return Path(f).with_suffix(".acb").name
+
+    def read(self, filename: str) -> bytes:
+        return (
+            (self.cwd / filename).read_bytes()
+            if self.args.format == "wav"
+            else subprocess.run(
+                f"ffmpeg -i {self.cwd / filename} -f {self.args.format} -b:a {self.args.bitrate}k pipe:1",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL,
+                check=True,
+            ).stdout
+        )
+
+
 if __name__ == "__main__":
 
     # ------------------------------ SETUP
-
-    logger = Logger()
 
     parser = ArgumentParser(
         description="Create a dataset for training a voice cloning model"
@@ -47,9 +115,6 @@ if __name__ == "__main__":
 
     # ------------------------------ SANITY CHECKS
 
-    logger.info("Fetching manifest...")
-    m = gom.fetch()
-
     if args.output == "":
         args.output = "".join(
             [
@@ -73,13 +138,7 @@ if __name__ == "__main__":
         args.output.parent.mkdir(parents=True, exist_ok=True)
 
     args.cache_dir = Path(args.cache_dir)
-    cache_active = False
-    if args.cache_dir.exists():
-        assert args.cache_dir.is_dir(), f"{args.cache_dir} is not a directory"
-        if list(args.cache_dir.iterdir()):
-            cache_active = True
-    else:
-        args.cache_dir.mkdir(parents=True, exist_ok=True)
+    sud_ch = SudCacheHandler(cwd=args.cache_dir, args=args)
 
     # ------------------------------ DOWNLOAD
 
@@ -92,27 +151,8 @@ if __name__ == "__main__":
         exit(1)
     logger.success(f"Found {len(target)} voice samples for '{args.character}'")
 
-    if cache_active:
-        logger.info("Computing cache diff...")
-        cached_names = [f.name for f in args.cache_dir.iterdir()]
-        target -= set(
-            [
-                Path("_".join(f.split("_")[:-1]) if f.startswith("sud_vo_adv_") else f)
-                .with_suffix(".acb")
-                .name
-                for f in cached_names
-            ]
-        )
-
-    logger.info(f"Downloading samples...")
-    m.download(
-        *sorted(list(target)),  # sort for logging
-        path=args.cache_dir,
-        categorize=False,
-        convert_audio=True,
-        audio_format="wav",  # avoid double compression
-        unpack_subsongs=True,
-    )
+    logger.info("Caching samples...")
+    sud_ch.cache(target)
 
     # ------------------------------ EXPORT
 
@@ -134,7 +174,7 @@ if __name__ == "__main__":
 
     logger.info("Exporting dataset...")
     if args.merge:
-        filelist_path = Path(args.cache_dir, "filelist.txt")
+        filelist_path = args.cache_dir / "filelist.txt"
         filelist_path.write_text(
             "".join([f"file '{f.name.replace("\\", "/")}'\n" for f in target_char])
         )
@@ -147,28 +187,17 @@ if __name__ == "__main__":
         with ZipFile(args.output, "w") as zipf:
             for f in tqdm(target_char):
                 dt = datetime.fromtimestamp(Path(f).stat().st_mtime)
-                content = (
-                    Path(f).read_bytes()
-                    if args.format == "wav"
-                    else subprocess.run(
-                        f"ffmpeg -i {f} -f {args.format} -b:a {args.bitrate}k pipe:1",
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.DEVNULL,
-                        check=True,
-                    ).stdout
-                )
                 zipf.writestr(
                     ZipInfo(
                         Path(f).with_suffix(f".{args.format}").name,
                         date_time=dt.timetuple()[:6],
                     ),
-                    content,
+                    sud_ch.read(f.name),
                 )
 
     # ------------------------------ CLEANUP
 
-    if not (cache_active or args.keep_cache):
-        logger.info("Purging cache...")
-        shutil.rmtree(args.cache_dir)
+    logger.info("Purging cache...")
+    sud_ch.purge()
 
     logger.success(f"Dataset ready at '{args.output}'")
