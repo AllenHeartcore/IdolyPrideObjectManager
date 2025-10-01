aab621c0d7b423d59c4a66ca9d20781526dab365
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-10-01 00:08:15 -0400

    [Fix] Improve update_manifest efficiency by fetching diff revisions in async

--- update_manifest.py
+++ update_manifest.py
@@ -4,13 +4,18 @@ Script to fetch latest manifest and diff from server,
 compatible with 'Update Manifest' workflow.
 """
 
+import asyncio
 import sys
 from pathlib import Path
 
 import IdolyPrideObjectManager as ipom
 
 
-def do_update(path: str) -> bool:
+def fetch_one(rev: int):
+    ipom.fetch(rev).export(f"manifests/v{rev:04}.json", force_overwrite=True)
+
+
+async def do_update(path: str) -> bool:
     """Check for manifest update from server and optionally update all diff revisions."""
 
     path = Path(path)
@@ -27,12 +32,13 @@ def do_update(path: str) -> bool:
     (path / "LATEST_REVISION").write_text(str(rev_remote))
 
     m_remote.export(path / "v0000.json", force_overwrite=True)
-    for i in range(1, rev_remote):
-        ipom.fetch(i).export(path / f"v{i:04}.json", force_overwrite=True)
+    await asyncio.gather(
+        *[asyncio.to_thread(fetch_one, i) for i in range(1, rev_remote)]
+    )
 
     return True
 
 
 if __name__ == "__main__":
-    HAS_UPDATE = do_update("manifests")
+    HAS_UPDATE = asyncio.run(do_update("manifests"))
     sys.exit(not (HAS_UPDATE))
